00c69206bb9d8b17418ac0e6b4ca3ef75e6daa83
Nick <nlehtela@uncc.edu>
2021-03-30 17:15:26 -0400

    Added zip search with results

--- routes/results.js
+++ routes/results.js
@@ -1,14 +1,25 @@
 var express = require("express");
 var router = express.Router();
 
+const documenu = require("documenu");
+documenu.configure("20f9d73a0a0f49f6ab39f1377d6728ef");
 
-//const fs = require("fs");
-const http = require("https");
-
+const defaultParams = {
+  size: 3,
+  page: 1,
+  fullmenu: false,
+};
 
 router.get("/", function (req, res) {
-  res.render("results");
+  documenu.Restaurants.getByZipCode(req.query.zipCode, defaultParams).then(
+    (result) => {
+      var restaurants = result.data;
+      res.render("results", {
+        allRestaurants: restaurants,
+        len: restaurants.length,
+      });
+    }
+  );
 });
 
-
-module.exports = router; 
+module.exports = router;
