0f3925ff5f9f9dd6c7a5928afbe6f50e49d7ac2c
Nick <nlehtela@uncc.edu>
2021-04-08 14:54:24 -0400

    Added navigation view and js

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ app.use(express.static(__dirname + "/"));
 
 var index = require("./routes/index");
 var results = require("./routes/results");
+var navigation = require("./routes/navigation");
 
 app.use("/", index);
 app.use("/results", results);
+app.use("/navigation", navigation);
 
 app.listen(8084);
